df7cef0cf8595ec57d1fda3cb9b09e5e9e828e2c
Esra Koç <esrakc239@gmail.com>
2024-02-29 11:12:45 +0300

    update

--- RecipeForm/src/components/Navbar.jsx
+++ RecipeForm/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import "../styleScss/navbar.css";
 import {Link, useNavigate} from "react-router-dom";
 import Logo from "../images/Logo.png";
